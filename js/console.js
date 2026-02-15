9384f19a365afb09f120858595b3a44b050399ee
bambukkan <pokko10@mail.ru>
2026-02-15 17:50:13 +0700

    split into files and consoles

---
+++ js/console.js
@@ -0,0 +1,11 @@
+const output = document.querySelector(".workspace__output");
+const input = document.querySelector(".workspace__input");
+
+input.addEventListener("keydown",(e) => {
+    if(e.key !== "Enter") return;
+    const cmd = input.value;
+    
+    output.innerHTML +=  `<div>> ${cmd}</div>`;
+    output.innerHTML += `<div>Команда "${cmd}" выполнена</div>`;
+    input.value = '';
+});
